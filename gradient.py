1799e305d28a01b6728676ee5aef40a1356c827d
Putu Gde Aditya Taguh Widiana <13517032@std.stei.itb.ac.id>
2020-03-20 06:19:01 +0700

    ann done

--- gradient.py
+++ gradient.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import math
+import random
+
+
+random.seed(1)
 
 def sigmoid(inputs, perceptron_weight):
     net = 0
@@ -24,15 +28,20 @@ def mini_batch_gradient(df,attribute,target_attribute):
     '''
     delta_weight = [[],[]]
     batch_size = 15
-    epoch = 1000
+    epoch = 186
+    maximum = -1
+    epoch_max = -1
 
     # initiate weight and delta weight with 0
     for i in range(len(perceptrons)-1):
         for _ in range(perceptrons[i]+1):
             weight[i].append([0 for _ in range(perceptrons[i+1])])
-            delta_weight[i].append([0 for _ in range(perceptrons[i+1])])
+            delta_weight[i].append([(random.random()*2)-1 for _ in range(perceptrons[i+1])])
     
-    
+    result = ['Setosa','Versicolor','Virginica']
+
+    prediction_result = []
+
     for _,row in df.iterrows():
         if row[target_attribute[0]] == 'Setosa':
             target_value.append([1,0,0])
@@ -42,7 +51,8 @@ def mini_batch_gradient(df,attribute,target_attribute):
             target_value.append([0,0,1])
     
     for ep in range(epoch):
-        print('\n\n EPOCH '+str(ep)+'\n')
+        print('EPOCH '+str(ep+1))
+        accuracy = 0
         current_batch_counter = 0
         for i,row in df.iterrows():
             # print(i)
@@ -99,20 +109,110 @@ def mini_batch_gradient(df,attribute,target_attribute):
                             delta_weight[i][j][k] = 0
                 current_batch_counter = 0
             # done
+            # if ep == epoch-1:
+            result_index = prediction_output.index(max(prediction_output))
             if ep == epoch-1:
-                print(str(prediction_output)+' <-- harusnya --> '+row[target_attribute[0]])
+                prediction_result.append(result[result_index])
+            if (result[result_index] == row[target_attribute[0]]):
+                accuracy += 1
+                # print(f'Prediction: {result[result_index]}\nReality: {row[target_attribute[0]]}\n\n')
+        # if ep == epoch-1:
+        if(accuracy > maximum):
+            maximum = accuracy
+            epoch_max = ep + 1
+        # print(f'accuracy = {accuracy}/150\n')
+
+    # print(f'\n\nepoch max : {epoch_max}\naccuracy = {maximum}/150')
+    # print(weight)
+    # for i in range(len(weight)):
+    #     print(f'\n\nlayer ke-{i+1}')
+
+    #     for j in range(len(weight[i])):
+    #         for k in range(len(weight[j])):
+    #             print(f'{j}-{k} : {weight[i][j][k]}')
+    return prediction_result
+
+def createConfusionMatrix(result, oldData, target_attribute):
+    # order : [[true positive, false positive, false negative, true negative]]
+    # order : [<Setosa>, <Versicolor>, <Virginica>]
+    # 0 : True positive
+    # 1 : False Positive
+    # 2 : False negative
+    # 3 : true negative
+    list_result = ['Setosa', 'Versicolor', 'Virginica']
+    cm = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    for i,row in oldData.iterrows():
+        true_result = row[target_attribute]
+        # print(true_result +' -> '+result[i])
+        if true_result == 'Setosa':
+            if result[i] == 'Setosa':
+                cm[0][0] += 1
+                cm[1][3] += 1
+                cm[2][3] += 1
+            elif result[i] == 'Versicolor':
+                cm[0][2] += 1
+                cm[1][1] += 1
+                cm[2][3] += 1
+            elif result[i] == 'Virginica':
+                cm[0][2] += 1
+                cm[1][3] += 1
+                cm[2][1] += 1
+            else:
+                print('Error when creating confusion matrix')
+                break
+        elif true_result == 'Versicolor':
+            if result[i] == 'Setosa':
+                cm[0][1] += 1
+                cm[1][2] += 1
+                cm[2][3] += 1
+            elif result[i] == 'Versicolor':
+                cm[0][3] += 1
+                cm[1][0] += 1
+                cm[2][3] += 1
+            elif result[i] == 'Virginica':
+                cm[0][3] += 1
+                cm[1][2] += 1
+                cm[2][1] += 1
+            else:
+                print('Error when creating confusion matrix')
+                break
+        elif true_result == 'Virginica':
+            if result[i] == 'Setosa':
+                cm[0][1] += 1
+                cm[1][3] += 1
+                cm[2][2] += 1
+            elif result[i] == 'Versicolor':
+                cm[0][3] += 1
+                cm[1][1] += 1
+                cm[2][2] += 1
+            elif result[i] == 'Virginica':
+                cm[0][3] += 1
+                cm[1][3] += 1
+                cm[2][0] += 1
+            else:
+                print('Error when creating confusion matrix')
+                break
+        else:
+            print('Error when creating confusion matrix')
+            break
+    print()
+    for i in range(len(list_result)):
+        print('Confusion Matrix For Class '+list_result[i])
+        print(f'True Positive : {cm[i][0]}')
+        print(f'False Positive : {cm[i][1]}')
+        print(f'False Negative : {cm[i][2]}')
+        print(f'True Negative : {cm[i][3]}')
+        print()
 
 if __name__ == '__main__':
     url = 'iris.csv'
     data = pd.read_csv(url)
     attributes = ['sepal_length','sepal_width','petal_length','petal_width']
     target_attribute = ['variety']
-    mini_batch_gradient(data,attributes,target_attribute)
+    prediction_result = mini_batch_gradient(data,attributes,target_attribute)
+    # print(prediction_result)
+    createConfusionMatrix(prediction_result,data,target_attribute[0])
     # assume one hidden layer with 4 neuron 
-
-
-
-
 
 # neuron layer:
 # 4 -> 4 -> 3
